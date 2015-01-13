126808f1db744e14cdde1537d8d720237af87f70
Sean Eagan <seaneagan1@gmail.com>
2015-01-13 11:14:05 -0600

    Add optional orElse parameters

--- lib/src/which_impl.dart
+++ lib/src/which_impl.dart
@@ -1,33 +1,48 @@
 
-library which.src.which;
+library which.src.which_impl;
 
 import 'dart:async';
-import 'dart:io';
 
 import 'package:when/when.dart';
 
-import 'is_executable.dart';
 import 'util.dart';
 
-Future<String> which(String command, Iterable<String> candidatePaths, bool isWindows, Future<bool> isExecutable(String path, bool isWindows)) => new Future(() => _which(
+Future<String> which(
+    String command,
+    Iterable<String> candidatePaths,
+    bool isWindows,
+    Future<bool> isExecutable(String path, bool isWindows),
+    orElse()) => new Future(() => _which(
     command,
     candidatePaths,
     isWindows,
     isExecutable,
+    orElse,
     toSequence: (items) => new Stream.fromIterable(items)));
 
-String whichSync(String command, Iterable<String> candidatePaths, bool isWindows, bool isExecutable(String path, bool isWindows)) => _which(
+String whichSync(
+    String command,
+    Iterable<String> candidatePaths,
+    bool isWindows,
+    bool isExecutable(String path, bool isWindows),
+    orElse()) => _which(
     command,
     candidatePaths,
     isWindows,
-    isExecutable);
+    isExecutable,
+    orElse);
 
-_which(String command, Iterable<String> candidatePaths, bool isWindows, isExecutable(String path, bool isWindows),
+_which(
+    String command,
+    Iterable<String> candidatePaths,
+    bool isWindows,
+    isExecutable(String path, bool isWindows),
+    orElse(),
     {toSequence(Iterable items): identity}) => when(
     () => firstWhere(
         toSequence(candidatePaths),
         (path) => isExecutable(path, isWindows),
-        orElse: () => _commandNotFound(command, null)),
+        orElse: orElse != null ? orElse : () => _commandNotFound(command, null)),
     identity,
     onError: (e) => _commandNotFound(command, e));
 
--- lib/which.dart
+++ lib/which.dart
@@ -8,8 +8,22 @@ import 'src/candidate_paths.dart';
 import 'src/is_executable.dart';
 import 'src/which_impl.dart' as impl;
 
-Future<String> which(String command) => new Future(() =>
-    impl.which(command, getRealCandidatePaths(command), Platform.isWindows, (path, isWindows) => isExecutable(path, isWindows, FileStat.stat)));
+/// Returns a future for the first [command] executable in the `PATH`.
+///
+/// If [command] is not found, [orElse] is called, which defaults to throwing.
+Future<String> which(String command, { orElse() }) => new Future(() => impl.which(
+    command,
+    getRealCandidatePaths(command),
+    Platform.isWindows,
+    (path, isWindows) => isExecutable(path, isWindows, FileStat.stat),
+    orElse));
 
-String whichSync(String command) =>
-    impl.whichSync(command, getRealCandidatePaths(command), Platform.isWindows, (path, isWindows) => isExecutableSync(path, isWindows, FileStat.statSync));
+/// Returns the first [command] executable in the `PATH`.
+///
+/// If [command] is not found, [orElse] is called, which defaults to throwing.
+String whichSync(String command, { orElse() }) => impl.whichSync(
+    command,
+    getRealCandidatePaths(command),
+    Platform.isWindows,
+    (path, isWindows) => isExecutableSync(path, isWindows, FileStat.statSync),
+    orElse);
