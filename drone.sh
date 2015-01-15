3dc815c529d0782631a46dd688f2ac10d7847b8b
Sean Eagan <seaneagan1@gmail.com>
2015-01-15 15:45:05 -0600

    Use test_runner

--- drone.sh
+++ drone.sh
@@ -2,20 +2,18 @@
 set -o xtrace
 
 pub get
-dart test/test.dart
+
+# TODO: Remove once https://github.com/drone/drone/issues/821 is fixed.
+export PATH="$PATH":"~/.pub-cache/bin"
+
+# Run tests.
+pub global activate test_runner
+test_runner -v
 
 # TODO: dartanalyzer on all libraries
 
 # Install dart_coveralls; gather and send coverage data.
 if [ "$REPO_TOKEN" ]; then
-  export PATH="$PATH":"~/.pub-cache/bin"
-
-  echo
-  echo "Installing dart_coveralls"
   pub global activate dart_coveralls
-
-  echo
-  echo "Running code coverage report"
-  # --debug for verbose logging
-  pub global run dart_coveralls report --token $REPO_TOKEN --retry 3 test/test.dart
+  dart_coveralls report --token $REPO_TOKEN --retry 3 test/test.dart
 fi
