37304b99038c6d1bd7c5455f150126e710b20420
Sean Eagan <seaneagan1@gmail.com>
2015-01-15 15:07:47 -0600

    Conform to test_runner conventions.

--- test/test.dart
+++ test/test.dart
@@ -1,11 +1,11 @@
 
 library which.test;
 
-import 'test_candidate_paths.dart' as candidate_paths;
-import 'test_has_permission.dart' as has_permission;
-import 'test_is_executable.dart' as is_exe;
-import 'test_which.dart' as which;
-import 'test_which_impl.dart' as which_impl;
+import 'candidate_paths_test.dart' as candidate_paths;
+import 'has_permission_test.dart' as has_permission;
+import 'is_executable_test.dart' as is_exe;
+import 'which_test.dart' as which;
+import 'which_impl_test.dart' as which_impl;
 
 main() {
   candidate_paths.main();
